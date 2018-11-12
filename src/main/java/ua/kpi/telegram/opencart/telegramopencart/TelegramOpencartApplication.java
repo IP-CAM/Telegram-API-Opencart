aeb7a597a143357a28b76a1a5b6b4a0bd68d55b6
Arsenii Venherak <indeooars@gmail.com>
2018-11-12 20:29:09 +0200

    master added gradle scripts

---
+++ src/main/java/ua/kpi/telegram/opencart/telegramopencart/TelegramOpencartApplication.java
@@ -0,0 +1,12 @@
+package ua.kpi.telegram.opencart.telegramopencart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramOpencartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramOpencartApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/kpi/telegram/opencart/telegramopencart/TelegramOpencartApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.kpi.telegram.opencart.telegramopencart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TelegramOpencartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
