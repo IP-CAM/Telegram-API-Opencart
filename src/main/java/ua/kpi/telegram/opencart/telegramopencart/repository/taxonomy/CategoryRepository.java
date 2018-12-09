f8b90edfc8963c726d3e807884eb51e7d49adcf2
Arsenii Venherak <indeooars@gmail.com>
2018-12-09 19:00:56 +0200

    dev feature: removed findAllByParentCategory from CategoryRepository

--- src/main/java/ua/kpi/telegram/opencart/telegramopencart/repository/taxonomy/CategoryRepository.java
+++ src/main/java/ua/kpi/telegram/opencart/telegramopencart/repository/taxonomy/CategoryRepository.java
@@ -4,11 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.kpi.telegram.opencart.telegramopencart.domain.model.taxonomy.Category;
 
-import java.util.List;
-
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByName(String name);
-
-    List<Category> findAllByParentCategory(Category category);
 }
