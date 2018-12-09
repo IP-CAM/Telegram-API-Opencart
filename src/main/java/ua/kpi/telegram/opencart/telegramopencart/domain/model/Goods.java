b0d8f1ff47a9e2b1f7f6343420b7b17756a95349
Arsenii Venherak <indeooars@gmail.com>
2018-12-09 16:15:14 +0200

    dev added category to Goods

--- src/main/java/ua/kpi/telegram/opencart/telegramopencart/domain/model/Goods.java
+++ src/main/java/ua/kpi/telegram/opencart/telegramopencart/domain/model/Goods.java
@@ -1,10 +1,22 @@
 package ua.kpi.telegram.opencart.telegramopencart.domain.model;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Goods extends TaxonomyUnit {
     private long price;
+
+    @ManyToOne
+    private Category category;
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 
     public long getPrice() {
         return price;
