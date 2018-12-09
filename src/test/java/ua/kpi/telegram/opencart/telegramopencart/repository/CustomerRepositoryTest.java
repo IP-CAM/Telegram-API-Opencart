b7c66ac3c7894ec9940be35a5e8d0c3ce964ed4e
Arsenii Venherak <indeooars@gmail.com>
2018-12-09 16:09:17 +0200

    dev added CustomerRepositoryTest

---
+++ src/test/java/ua/kpi/telegram/opencart/telegramopencart/repository/CustomerRepositoryTest.java
@@ -0,0 +1,50 @@
+package ua.kpi.telegram.opencart.telegramopencart.repository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+import ua.kpi.telegram.opencart.telegramopencart.domain.model.Customer;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CustomerRepositoryTest {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    private static final String TEST_LOGIN = "TestLogin";
+
+    private static final String TEST_PHONE = "+3801235454";
+
+    private Customer savedCustomer = new Customer();
+
+    @Before
+    public void setUp() {
+        savedCustomer.setLogin(TEST_LOGIN);
+        savedCustomer.setPhone(TEST_PHONE);
+
+        testEntityManager.persist(savedCustomer);
+    }
+
+    @Test
+    public void shouldReturnCorrectCustomerOnFindingByLogin() {
+        Customer customer = customerRepository.findCustomerByLogin(TEST_LOGIN);
+
+        assertEquals(savedCustomer, customer);
+    }
+
+    @Test
+    public void shouldReturnCorrectCustomerOnFindingByPhone() {
+        Customer customer = customerRepository.findCustomerByPhone(TEST_PHONE);
+
+        assertEquals(savedCustomer, customer);
+    }
+}
