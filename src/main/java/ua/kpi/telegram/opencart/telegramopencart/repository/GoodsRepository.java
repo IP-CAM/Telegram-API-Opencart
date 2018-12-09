fb8334591cac44a9f9a2b6dbdff71b2bfdcdbad7
Arsenii Venherak <indeooars@gmail.com>
2018-12-09 16:15:54 +0200

    dev added findByCategory and findByName methods to GoodsRepository

--- src/main/java/ua/kpi/telegram/opencart/telegramopencart/repository/GoodsRepository.java
+++ src/main/java/ua/kpi/telegram/opencart/telegramopencart/repository/GoodsRepository.java
@@ -1,7 +1,11 @@
 package ua.kpi.telegram.opencart.telegramopencart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ua.kpi.telegram.opencart.telegramopencart.domain.model.Category;
 import ua.kpi.telegram.opencart.telegramopencart.domain.model.Goods;
 
 public interface GoodsRepository extends JpaRepository<Goods, Long> {
+    Goods findByName(String name);
+
+    Goods findByCategory(Category category);
 }
