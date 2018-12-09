c5c174e7a713e3e79b13d1c43193cc445ab2c7b5
Arsenii Venherak <indeooars@gmail.com>
2018-12-09 16:29:24 +0200

    dev feature: added CategoryRepository

--- src/main/java/ua/kpi/telegram/opencart/telegramopencart/repository/CategoryRepository.java
+++ src/main/java/ua/kpi/telegram/opencart/telegramopencart/repository/CategoryRepository.java
@@ -3,13 +3,12 @@ package ua.kpi.telegram.opencart.telegramopencart.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.kpi.telegram.opencart.telegramopencart.domain.model.Category;
-import ua.kpi.telegram.opencart.telegramopencart.domain.model.TaxonomyUnit;
 
 import java.util.List;
 
 @Repository
-public interface TaxonomyUnitRepository extends JpaRepository<TaxonomyUnit, Long> {
-    List<TaxonomyUnit> findAllByParentCategory(Category category);
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String name);
 
-    TaxonomyUnit findByName(String name);
+    List<Category> findAllByParentCategory(Category category);
 }
