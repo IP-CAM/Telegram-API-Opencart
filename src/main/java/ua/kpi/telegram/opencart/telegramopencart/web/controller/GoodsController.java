4fab32e2edac930fd875d0f268dfa4a4506ea050
Arsenii Venherak <indeooars@gmail.com>
2018-12-25 17:06:34 +0200

    dev added findAll and findAllByCategories

--- src/main/java/ua/kpi/telegram/opencart/telegramopencart/web/controller/GoodsController.java
+++ src/main/java/ua/kpi/telegram/opencart/telegramopencart/web/controller/GoodsController.java
@@ -1,7 +1,12 @@
 package ua.kpi.telegram.opencart.telegramopencart.web.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import ua.kpi.telegram.opencart.telegramopencart.domain.model.taxonomy.Goods;
 import ua.kpi.telegram.opencart.telegramopencart.domain.service.GoodsService;
+
+import java.util.List;
 
 @RestController
 public class GoodsController {
@@ -9,5 +14,15 @@ public class GoodsController {
 
     public GoodsController(GoodsService goodsService) {
         this.goodsService = goodsService;
+    }
+
+    @GetMapping("/goods")
+    public List<Goods> findAll() {
+        return goodsService.getAll();
+    }
+
+    @GetMapping("/goods/{category}")
+    public List<Goods> findByCategory(@PathVariable("category") String category) {
+        return goodsService.getAllByCategory(category);
     }
 }
