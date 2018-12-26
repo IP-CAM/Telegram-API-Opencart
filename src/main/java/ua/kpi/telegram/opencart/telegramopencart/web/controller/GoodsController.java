652a1834bb4f9fd8332e8b4915e51874b87c000d
Arsenii Venherak <indeooars@gmail.com>
2018-12-26 23:00:54 +0200

    dev added more Goods to Controller

--- src/main/java/ua/kpi/telegram/opencart/telegramopencart/web/controller/GoodsController.java
+++ src/main/java/ua/kpi/telegram/opencart/telegramopencart/web/controller/GoodsController.java
@@ -1,14 +1,20 @@
 package ua.kpi.telegram.opencart.telegramopencart.web.controller;
 
 import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ua.kpi.telegram.opencart.telegramopencart.domain.model.taxonomy.Goods;
 import ua.kpi.telegram.opencart.telegramopencart.domain.service.GoodsService;
 
 import java.util.List;
 
+import static java.lang.Long.parseLong;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @RestController
@@ -31,5 +37,21 @@ public class GoodsController {
     public List<Goods> findByCategory(@PathVariable("category") String category) {
         logger.info("Finding all goods by category");
         return goodsService.getAllByCategory(category);
+    }
+
+    @PutMapping("/goods")
+    public Goods createGoods(@RequestBody Goods goods) {
+        return goodsService.add(goods);
+    }
+
+    @PostMapping("/goods")
+    public Goods updateGoods(@RequestBody Goods goods,
+                             @RequestParam("id") String id) {
+        return goodsService.update(goods, parseLong(id));
+    }
+
+    @DeleteMapping("/goods")
+    public void deleteGoods(@RequestParam("name") String name) {
+        goodsService.remove(name);
     }
 }
